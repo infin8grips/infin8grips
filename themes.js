65cd6f41a6295c404a87f01cd768b3d4aa08a5d4
infin8grips <infin8grips@gmail.com>
2026-03-10 12:27:23 -0500

    Add BRAND theme configuration

--- themes.js
+++ themes.js
@@ -1,1 +1,12 @@
+export const BRAND = {
+  name: "Infin8 Grips",
+
+  primary: "#000000",
+  accent: "#6C47FF",
+  bg: "#F8F8F8",
+  text: "#111111",
+  muted: "#777777",
+
+  font: "Inter, sans-serif"
+};
 
