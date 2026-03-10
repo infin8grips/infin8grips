2a0985ae097b7500851a75ce1989225ec6c4f4c5
infin8grips <infin8grips@gmail.com>
2026-03-10 12:35:35 -0500

    Add Thank You page component

---
+++ pages/thankyou.js
@@ -0,0 +1,10 @@
+export default function ThankYou() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-center">
+      <h1 className="text-5xl font-bold">Thank You!</h1>
+      <p className="mt-4 text-gray-600 text-lg">
+        Your order has been received.
+      </p>
+    </div>
+  );
+}
